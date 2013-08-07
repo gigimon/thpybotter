71839b36f7a462ca1f1f2609d1ba945078cede9a
Darwin <darwin.ggl@gmail.com>
2013-08-07 18:04:08 +0400

    add https vk parsing

--- example_config.py
+++
@@ -1,6 +0,0 @@
-NICKNAME = "gigibot"
-REALNAME = "gigibot"
-
-SERVER = ("test.irc.ru", 6667)
-CHANNELS = ["#chan1", "#chan2"]
-
--- plugins/irc_vk.py
+++ plugins/irc_vk.py
@@ -8,7 +8,7 @@ from bs4 import BeautifulSoup
 class IRCPlugin(BasePlugin):
     name = "vkontakte"
     enabled = True
-    reg = re.compile(r'(http://vk.com/wall[-\d_\d]+)')
+    reg = re.compile(r'((?:http|https)://vk.com/wall[-\d_\d]+)')
 
     def _validate(self, event):
         if self.is_pubmsg(event):
@@ -20,15 +20,15 @@ class IRCPlugin(BasePlugin):
             yield message[i:i+300]
       
     def _run(self, msg):
-        
-        url = self.reg.findall(' '.join(msg[1].arguments))[0]
-        try:
-            soup = BeautifulSoup(requests.get(url).content)
-            post_text = soup.find("div", class_="wall_post_text").text
-            author = soup.find("a", class_="fw_post_author").text
+        urls = self.reg.findall(' '.join(msg[1].arguments))
+        for url in urls:
+            try:
+                soup = BeautifulSoup(requests.get(url).content)
+                post_text = soup.find("div", class_="wall_post_text").text
+                author = soup.find("a", class_="fw_post_author").text
 
-            pretty_text = self.colorize(author, post_text)
-            for text in self._split(pretty_text):
-                msg[0].privmsg(msg[1].target, text)
-        except:
-            pass
+                pretty_text = self.colorize(author, post_text)
+                for text in self._split(pretty_text):
+                    msg[0].privmsg(msg[1].target, text)
+            except:
+                pass
