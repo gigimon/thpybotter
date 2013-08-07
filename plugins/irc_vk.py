1aeb4ae9da5f4cb71f3d053bba7db2d982bd54e1
Darwin <darwin.ggl@gmail.com>
2013-08-07 14:19:00 +0400

    delete debug prints

--- plugins/irc_vk.py
+++ plugins/irc_vk.py
@@ -15,7 +15,6 @@ class IRCPlugin(BasePlugin):
     def _validate(self, event):
         if self.is_pubmsg(event):
             if 'vk.com' in ' '.join(event.arguments):
-                print "got request"
                 return True
         return False
     def _split(self, message):
@@ -25,16 +24,12 @@ class IRCPlugin(BasePlugin):
     def _run(self, msg):
         
         url = self.reg.findall(' '.join(msg[1].arguments))[0]
-        print msg[1].arguments
-        #for url in urls:
-        print "url: ", url
         try:
             soup = BeautifulSoup(requests.get(url).content)
             post_text = soup.find("div", class_="wall_post_text").text
             author = soup.find("a", class_="fw_post_author").text
 
             pretty_text = self.colorize(author, post_text)
-            #if len(pretty_text) <= 512:
             for text in self._split(pretty_text):
                 msg[0].privmsg(msg[1].target, text)
         except:
